2eb06a81a47c4083b0756a9f8dd5de5b13584cb5
raghuveer9303 <34271141+raghuveer9303@users.noreply.github.com>
2025-03-25 19:55:22 -0400

    Prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/SII-Sports-Data-Solutions-Lab/LDA-TreadBike-Sentiment/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
