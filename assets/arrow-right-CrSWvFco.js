d91ea8bd1e7ea6ad152dde174c31ca8a7a500166
raghuveer9303 <raghuveer.9303@gmail.com>
2025-04-25 01:37:32 -0400

    Updates

--- assets/arrow-right-CrSWvFco.js
+++ assets/arrow-right-CrSWvFco.js
@@ -1,4 +1,4 @@
-import{b as r}from"./index-CBXF-sCb.js";/**
+import{b as r}from"./index-BP_PIMbX.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
